725ed191285914a5a29c2d43aa1a3f474c9c75d5
Pablo López Landeros <pablo.lopez.landeros@hotmail.com>
2020-12-08 14:21:12 -0600

    Todo funciona

--- Soccer/poisson_sims_pvc.R
+++ Soccer/poisson_sims_pvc.R
@@ -6,6 +6,8 @@ library(gganimate)
 library(gifski)
 library(janitor)
 library(shadowtext)
+library(ggtext)
+library(ggthemes)
 
 #Pumas vs azul
 #adjusting for h2h record:
@@ -55,7 +57,7 @@ sim_matches <- function(n){
 #------------------------------------------------------------------------------
 
 #simulamos 10000 veces
-simulacion <- sim_matches(100)
+simulacion <- sim_matches(10000)
 tabla <- as.data.frame(table(simulacion$avanza))
 
 #para animar:
@@ -69,7 +71,7 @@ simulacion <- simulacion %>%
 pumas <- as.data.frame(cbind(simulacion$iteracion,simulacion$vic_pumas))
 caz <- as.data.frame(cbind(simulacion$iteracion,simulacion$vic_caz))
 pumas$logo <- "#303af1"
-caz$logo <- "cbab57"
+caz$logo <- "#cbab57"
 pumas$equipo <- "UNAM"
 caz$equipo <- "CAZ"
 
@@ -81,22 +83,78 @@ df_gif[cols.num] <- sapply(df_gif[cols.num],as.numeric)
 sapply(df_gif, class)
 
 #con menos numeros
-iters <- seq(from = 0, to = 100, by = 5)
+iters <- seq(from = 0, to = 10000, by = 500)
 r_df <- df_gif[df_gif$iteracion %in% iters,]
 
 
-#PLOT
+#PLOT con escala logaritmica:------------------------------------------------------
 ggplot(data = df_gif,
        mapping = aes(x=iteracion,
                      y=wins,
+                     color = logo,
                      group = equipo))+
   geom_point()+
+  geom_line()+
   scale_x_continuous(limits = c(0,max(df_gif$iteracion)))+
+  scale_y_log10(expand = expansion(add = c(0,0.1)), 
+                breaks=c(20,50,100,150, 500, 1000, 2000, 5000,7000, 10000)) +
+  theme_minimal() +
+  scale_color_manual(values=c("#cbab57", "#303af1"))+
   shadowtext::geom_shadowtext(data = df_gif,
                               mapping = aes(x=iteracion,
                                             y=wins,
                                             label = equipo),
                               hjust=-0.1, vjust = 0, bg.color = "black")+
+  theme(
+    plot.title = element_text(size = 18, family = "Trebuchet MS",color = "grey20",hjust = 0),
+    plot.subtitle = element_text(size = 13, family = "Trebuchet MS",color = "grey20",hjust = 0),
+    axis.title = element_text(size = 10, family = "Trebuchet MS",color = "grey20"),
+    panel.grid.minor = element_blank(),
+    legend.position = "none",
+    plot.margin = margin(3,15,3,3,"mm"),
+    plot.caption = ggtext::element_markdown()
+  )+
   coord_cartesian(clip = "off") +
-  transition_reveal(iteracion)
+  labs(x = "Numero de Simulacion", 
+       y = "Pases a siguiente ronda (En escala logarítmica)",
+       title = "Simulacion Monte Carlo de 10,000 partidos",
+       subtitle =  "Posibilidades de pase de Cruz Azul ~ 40:1",
+       caption = "<span style = 'font-size:8pt;color:#888888'>Data Source: FB REF <br> Plot: Pablo L. Landeros </span>") +
+  transition_reveal(iteracion) 
 #colors "#cbab57" "#303af1"
+anim_save('C:/Users/pablo/Desktop/GithubRepos/Experiments/Soccer/pumas_v_azul_log.gif')
+
+#Sin logaritmica:--------------------------------------------------------
+ggplot(data = df_gif,
+       mapping = aes(x=iteracion,
+                     y=wins,
+                     color = logo,
+                     group = equipo))+
+  geom_point()+
+  geom_line()+
+  scale_x_continuous(limits = c(0,max(df_gif$iteracion)))+
+  theme_fivethirtyeight() +
+  scale_color_manual(values=c("#cbab57", "#303af1"))+
+  shadowtext::geom_shadowtext(data = df_gif,
+                              mapping = aes(x=iteracion,
+                                            y=wins,
+                                            label = equipo),
+                              hjust=-0.1, vjust = 0, bg.color = "black")+
+  theme(
+    plot.title = element_text(size = 18, family = "Trebuchet MS",color = "grey20",hjust = 0),
+    plot.subtitle = element_text(size = 13, family = "Trebuchet MS",color = "grey20",hjust = 0),
+    axis.title = element_text(size = 12, family = "Trebuchet MS",color = "grey20"),
+    panel.grid.minor = element_blank(),
+    legend.position = "none",
+    plot.margin = margin(3,15,3,3,"mm"),
+    plot.caption = ggtext::element_markdown()
+  )+
+  coord_cartesian(clip = "off") +
+  labs(x = "Numero de Simulacion", 
+       y = "Pases a siguiente ronda",
+       title = "Simulacion Monte Carlo de 10,000 partidos",
+       subtitle =  "Probabilidad de pase de Cruz Azul: 97.8 %",
+       caption = "<span style = 'font-size:8pt;color:#888888'>Data Source: FB REF <br> Plot: Pablo L. Landeros </span>") +
+  transition_reveal(iteracion) 
+#colors "#cbab57" "#303af1"
+anim_save('C:/Users/pablo/Desktop/GithubRepos/Experiments/Soccer/pumas_v_azul.gif')
